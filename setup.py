0224f9bc55012df11622cda06f03690603c329c2
Ben Woodcroft <donttrustben@gmail.com>
2017-01-28 21:22:11 +1000

    setup.py: Support 'test'.

--- setup.py
+++ setup.py
@@ -5,12 +5,19 @@ from setuptools import setup, find_packages
 
 exec(open('version.py').read()) # loads __version__
 
+import unittest
+def my_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('test', pattern='test_*.py')
+    return test_suite
+
 setup(name='extern',
       version=__version__,
       author='Ben Woodcroft',
       author_email='donttrustben@gmail.com',
-    description='',
-    long_description=open('README.md').read(),
-    license='MIT',
-    keywords="",
-    packages= find_packages(exclude='docs'))
+      description='',
+      long_description=open('README.md').read(),
+      license='MIT',
+      keywords="",
+      test_suite='setup.my_test_suite',
+      packages= find_packages(exclude='docs'))
